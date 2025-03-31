cdc067df5e1aa2ef4fd8842a617fd4df93b04f7c
Shimmi Shikha <47597931+ShimmiShikhaPrashi@users.noreply.github.com>
2025-03-31 13:40:54 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,35 +1,4 @@
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const PORT = 3000;
-
-  return {
-    server: {
-      open: true,
-      port: PORT
-    },
-    define: {
-      global: 'window'
-    },
-    css: {
-      preprocessorOptions: {
-        scss: { charset: false },
-        less: { charset: false }
-      },
-      charset: false,
-      postcss: {
-        plugins: [
-          {
-            postcssPlugin: 'internal:charset-removal',
-            AtRule: {
-              charset: (atRule) => {
-                if (atRule.name === 'charset') atRule.remove();
-              }
-            }
-          }
-        ]
-      }
-    },
-    base: '/Employment-Engagement-Dashboard/', // Explicitly match GitHub repo name
-    plugins: [react(), jsconfigPaths()]
-  };
+export default defineConfig({
+  base: '/Employment-Engagement-Dashboard/', // Match your repository name exactly!
+  plugins: [react(), jsconfigPaths()],
 });
